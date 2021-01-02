31524925315dd9bab12cdab8878b25a86a8844c0
Ganesh Kumar Dheivasigamani <dganeshkumar@users.noreply.github.com>
2021-01-02 12:26:47 -0500

    Add files via upload

--- #DailyCoding.py
+++ #DailyCoding.py
@@ -1,4 +1,4 @@
-#Day 1
+#Day 1- 01 Jan 2021
 print('*'*25, 'Start of Day 1', "*"*25)
 print('We will learn about the following functions: print(), type(), len(), input(), str(), int()')
 print('We will also learn about string concatenation, string replication.')
@@ -18,7 +18,7 @@ print(name + ', you will be ' + str(age+1) + ' in one year') #Converting age to 
 
 print('*'*25, 'End of Day 1', "*"*25)
 
-# Day 2
+# Day 2- 02 Jan 2021
 # Multiline statements- Statements in Python can be extended to one or more lines using () {} [] ; \
 
 #Declared using \
